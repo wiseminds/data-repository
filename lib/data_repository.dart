03a7a9804917c25ac30af5855df2afe3c88bd6d2
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2022-04-04 23:28:28 +0100

    added json utils

--- lib/data_repository.dart
+++ lib/data_repository.dart
@@ -1,7 +1,89 @@
-library data_repository;
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/foundation.dart';
+
+import 'cache/cache_description.dart';
+import 'cache/cache_mixin.dart';
+import 'local/local_repository.dart';
+import 'remote/provider/api_request.dart';
+import 'remote/provider/api_response.dart';
+import 'remote/remote_repository.dart';
+import 'utils/api_config.dart';
+import 'utils/exception_formater.dart';
+import 'utils/json_utlis.dart';
+
+typedef NetworkCall<T> = Future<T> Function();
+
+/// Data repository class that handles all the network calls an local data access
+abstract class DataRepository with ExceptionFormater, CacheMixin {
+  final LocalRepository localRepository;
+  final RemoteRepository remoteRepository;
+  final String? defaultErrorMessage;
+
+  DataRepository(this.localRepository, this.remoteRepository,
+      [this.defaultErrorMessage]) {
+    ApiConfig().defaultErrorMessage =
+        defaultErrorMessage ?? ApiConfig().defaultErrorMessage;
+  }
+
+  /// manages fetching data, decides where to fetch data from
+  Future<ApiResponse<ResultType, Item>> handleRequest<ResultType, Item>(
+      NetworkCall<ApiResponse<ResultType, Item>> networkCall,
+      {CacheDescription? cache,
+      int timeout = 50,
+      bool retryWithCache = false,
+      bool retry = false}) async {
+    bool _useCache = await shouldUseCache(localRepository, cache);
+
+    /// fetches data from cache if a valid cached data exists
+    if (_useCache) {
+      if (kDebugMode) print('fetching data from cache');
+      var data = await localRepository.getData(cache!.key);
+      // data = JsonInterceptor.convertFromJson<ResultType, Item>(data);
+      // print('${data != null && data is ResultType}');
+      // if (kDebugMode) print('fetching data from cache $data');
+      if (data != null && data is ResultType) {
+        return ApiResponse<ResultType, Item>(
+            request: ApiRequest.dummy(),
+            bodyString: data.toString(),
+            // body: data,
+            headers: {},
+            statusCode: 210).resolve;
+      }
+    }
+
+    /// else fetches data from the remote source
+    ApiResponse<ResultType, Item> response = await remoteRepository
+        .handleRequest<ResultType, Item>(networkCall, timeout: timeout);
+
+    // print('headers: ${response.request.headers}');
+
+    if (!response.isSuccessful) {
+      response = remoteRepository.handleError(response);
+      if (retryWithCache && cache != null) {
+        return handleRequest(networkCall,
+            cache: cache.copyWith(overrideTime: true));
+      }
+    }
+
+    if (kDebugMode) print(response.error);
+    if (kDebugMode) print(' finished request');
+
+    if (cache != null &&
+        !cache.ignoreSave &&
+        cache.key.isNotEmpty &&
+        response.body != null) {
+      var data = validateData<ResultType, Item>(response);
+      if (data != null) {
+        String json = Jsonutils.convertToJson(data);
+        localRepository.saveData(cache.key, json);
+        localRepository.saveTime(
+            cache.key,
+            DateTime.now()
+                .add(Duration(milliseconds: cache.lifeSpan))
+                .millisecondsSinceEpoch);
+      }
+    }
+    return response;
+  }
 }
---
+++ lib/utils/json_utlis.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class Jsonutils {
+  static String encode(dynamic json) {
+    return jsonEncode(json);
+  }
+
+  static dynamic decode(String json) {
+    return jsonDecode(json);
+  }
+ 
+
+   static dynamic convertToJson(dynamic data) {
+    return json.encode(_encode(data));
+  }
+
+  static dynamic _serialize<T>(dynamic value) {
+    if(value is Map<String, dynamic> || value is String) return value;
+    try {
+      if(value.toJson != null) return value.toJson();
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  static List<T> _serializeListOf<T>(Iterable value) => List.castFrom(
+        value.map((value) => _serialize<T>(value)).toList(growable: false),
+      );
+
+  static dynamic _encode<T>(entity) {
+    /// handle case when we want to access to Map<String, dynamic> directly
+    /// getResource or getMapResource
+    /// Avoid dynamic or unconverted value, this could lead to several issues
+    if (entity is String) return entity;
+
+    try {
+      if (entity is Iterable) return _serializeListOf<T>(entity);
+      return _serialize<T>(entity);
+    } catch (e) {
+      if (kDebugMode) print(e);
+      return null;
+    }
+  }
+}
