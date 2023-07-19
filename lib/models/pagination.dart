e31cbfcec153f07bf2d70f4014465b3921d0469d
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2023-07-19 19:51:56 +0100

    removed chunk count

--- lib/models/pagination.dart
+++ lib/models/pagination.dart
@@ -1,10 +1,9 @@
 class Pagination {
-  final int total, chunkCount, limit, pages, page;
+  final int total, limit, pages, page;
   final String? order, query;
 
   Pagination(
       {this.total = 0,
-      this.chunkCount = 0,
       this.limit = 0,
       this.pages = 0,
       this.page = 1,
