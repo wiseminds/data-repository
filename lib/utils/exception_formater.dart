9e98a837cbb3319c5421897d460660c96b14629e
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2025-04-29 00:28:49 +0100

    fixed network error, upgraded packages and constraint

--- lib/utils/exception_formater.dart
+++ lib/utils/exception_formater.dart
@@ -15,49 +15,49 @@ mixin ExceptionFormater {
     int? code;
     // if (kDebugMode) print(error is ApiError);
     if (kDebugMode) print('formatErrorMessage, ${error.runtimeType}');
-    switch (error.runtimeType) {
+    switch (error.runtimeType.toString()) {
       case ApiError:
         message = error.message ?? defaultErrorMessage;
         code = error.code.toInt() ?? 1000;
-        break;
-      case SocketException:
-      case HttpException:
-      case RedirectException:
-      case WebSocketException:
+        break;  
+      case 'SocketException':
+      case 'HttpException':
+      case 'RedirectException':
+      case 'WebSocketException':
         message = 'Please check your internet connection and try again';
         // 'Could not connect to the server please check your internet connection';
         code = 7000;
         break;
-      case FormatException:
+      case 'FormatException':
         message = 'Improperly formatted value';
         code = 7001;
         break;
-      case MissingPluginException:
+      case 'MissingPluginException':
         message = 'Plugin not found';
         code = 7002;
         break;
-      case NetworkImageLoadException:
+      case 'NetworkImageLoadException':
         message = 'Could not load the image image';
         code = 7003;
         break;
-      case HandshakeException:
+      case 'HandshakeException':
         message = 'Could not establish secure connection with the server';
         code = 7004;
         break;
-      case CertificateException:
+      case 'CertificateException':
         message = 'An error ocurred could not verify server certificate';
         code = 7005;
         break;
-      case FileSystemException:
+      case 'FileSystemException':
         message = 'A filesystem exception has occurred';
         code = 7006;
         break;
-      case TlsException:
+      case 'TlsException':
         message = 'SSL error occurred ${error?.message ?? ''}';
         code = 7007;
         break;
 
-      case TimeoutException:
+      case 'TimeoutException':
         message = 'Connection Timed out please check your internet connection';
         code = 7009;
         break;
